ca1faa1761e9439d29d19b16ea4560989af17e55
Thomas Vachuska <tom@opennetworking.org>
2019-06-19 14:23:13 -0700

    Fixed compiler warning.

--- ESPGizmoDefault.h
+++ ESPGizmoDefault.h
@@ -6,7 +6,7 @@ ESPGizmo gizmo;
 
 void defaultMqttCallback(char *topic, uint8_t *payload, unsigned int length) {
     char value[64];
-    value[0] = NULL;
+    value[0] = '\0';
     strncat(value, (char *) payload, length);
     Serial.printf("%s: %s\n", topic, value);
     gizmo.handleMQTTMessage(topic, value);
